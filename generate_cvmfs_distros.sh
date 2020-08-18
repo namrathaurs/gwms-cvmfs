bfd316fa6bb0141a9c8d704b309d44e44dd6291a
namrathaurs <namrathaurs@my.unt.edu>
2020-08-18 17:38:06 -0500

    modified (commit message enclosed)
    File ‘generate_cvmfs_distros.sh’
    	- added creation of tarball containing the generated cvmfs distributions
    	- updated the script to generate distributions for both osg and egi sources without being dependent on the command line arguments
    	- improved logging to indicate success or failure status for distributions as and when they are created with makedist

--- generate_cvmfs_distros.sh
+++ generate_cvmfs_distros.sh
@@ -7,16 +7,26 @@ usage() {
 	echo "the source to download the latest cvmfs configuration and repositories."
 }
 
-
 # parameter is 'osg', 'egi' or 'default' to download the latest cvmfs and configuration
 # rpm from one of those three sources (Ref. https://www.github.com/cvmfs/cvmfsexec)
-cvmfs_src=$1
+#cvmfs_src=$1
+# uncomment the following if block if the source is passed as a command line argument
+#if [[ ! $cvmfs_src =~ ^(osg|egi|default)$ ]]; then
+#	echo "Invalid command line argument: Must be one of {osg|egi|default}"
+#	exit 1
+#fi
+
+CVMFS_SOURCES=osg:egi
+SUPPORTED_TYPES=rhel6-x86_64:rhel7-x86_64:rhel8-x86_64:suse15-x86_64
 cvmfsexec_temp=/tmp/cvmfsexec_pkg
 cvmfsexec_base=$cvmfsexec_temp/cvmfsexec
+cvmfs_distros=$cvmfsexec_temp/distros
 
-if [[ ! $cvmfs_src =~ ^(osg|egi|default)$ ]]; then
-	echo "Invalid command line argument: Must be one of {osg|egi|default}"
-	exit 1
+if [[ ! -d $cvmfsexec_temp ]]; then
+	mkdir -p $cvmfsexec_temp
+	mkdir -p $cvmfs_distros
+elif [[ -d $cvmfsexec_temp && ! -d $cvmfs_distros ]]; then
+	mkdir -p $cvmfs_distros
 fi
 
 if [[ ! -d $cvmfsexec_base ]]; then
@@ -26,22 +36,40 @@ elif [[ -d $cvmfsexec_base && -d $cvmfsexec_base/dist ]]; then
 fi
 #echo $?
 
-supported_types=rhel6-x86_64:rhel7-x86_64:rhel8-x86_64:suse15-x86_64
+declare -a repo_sources
+repo_sources=($(echo $CVMFS_SOURCES | tr ":" "\n"))
+
 declare -a avail_types
-avail_types=($(echo $supported_types | tr ":" "\n"))
+avail_types=($(echo $SUPPORTED_TYPES | tr ":" "\n"))
 #echo ${avail_types[@]}       
 
-for type in "${avail_types[@]}"
-do
-	echo "Making $cvmfs_src distribution for $type machine..."
-	os=`echo $type | awk -F'-' '{print $1}'`
-	arch=`echo $type | awk -F'-' '{print $2}'`		
-	$cvmfsexec_base/makedist -m $type $cvmfs_src &>> $cvmfsexec_temp/gen_distros.log 
-	$cvmfsexec_base/makedist -o $cvmfsexec_temp/cvmfsexec-${cvmfs_src}-${os}-${arch} &>> $cvmfsexec_temp/gen_distros.log
 
-	# delete the dist directory within cvmfsexec to download the cvmfs configuration
-	# and repositories for another machine type
-	rm -rf $cvmfsexec_base/dist
+for cvmfs_src in "${repo_sources[@]}"
+do
+	for avail_type in "${avail_types[@]}"
+	do
+		echo -n "Making $cvmfs_src distribution for $avail_type machine..."
+		os=`echo $avail_type | awk -F'-' '{print $1}'`
+		arch=`echo $avail_type | awk -F'-' '{print $2}'`		
+		$cvmfsexec_base/makedist -m $avail_type $cvmfs_src &>> $cvmfsexec_temp/gen_distros.log 
+		$cvmfsexec_base/makedist -o $cvmfs_distros/cvmfsexec-${cvmfs_src}-${os}-${arch} &>> $cvmfsexec_temp/gen_distros.log
+		
+		if [[ -e $cvmfs_distros/cvmfsexec-${cvmfs_src}-${os}-${arch} ]]; then
+			echo " Success"
+		else
+			echo " Failed! Check $cvmfsexec_temp/gen_distros.log for details."
+		fi
+			
+		# delete the dist directory within cvmfsexec to download the cvmfs configuration
+		# and repositories for another machine type
+		rm -rf $cvmfsexec_base/dist
+	done
 done
 
-echo "cvmfsexec distributions are available in $cvmfsexec_temp"
+echo -e "\nCreating a tarball with generated distributions for all supported machine types..."
+tar -cvzf $cvmfsexec_temp/cvmfs_distros.tar.gz -C $cvmfsexec_temp distros &>> $cvmfsexec_temp/gen_distros.log
+
+# check if the tarball has all the distros previously created!
+tar -tvzf $cvmfsexec_temp/cvmfs_distros.tar.gz
+
+echo "Tarball containing cvmfsexec distributions is now available in $cvmfsexec_temp"
