8b88321c9d4cb19f58e5b4cae2408528a9c6c6d5
Namratha <namrathaurs@my.unt.edu>
2021-07-20 16:34:35 +0000

    refactored code after debugging
    - removed old echo statements used for debugging
    - added echo statement to track the value of GWMS_IS_CVMFS_MNT variable
    - added fetching of GLIDEIN_USE_CVMFSEXEC attribute value from the config file
    - removed invocation for unmounting part of the workflow; now handled at the time of glidein cleanup

--- cvmfs_test.sh
+++ cvmfs_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo $1
-echo $#
+#echo $1
+#echo $#
 # first parameter passed to this script will always be the glidein configuration file (glidein_config)
 glidein_config=$1
 
@@ -12,6 +12,12 @@ error_gen=`grep '^ERROR_GEN_PATH ' $glidein_config | awk '{print $2}'`
 # get the glidein work directory location from glidein_config file
 work_dir=`grep '^GLIDEIN_WORK_DIR ' $glidein_config | awk '{print $2}'`
 #echo "Glidein Work Directory: $work_dir"
+
+# get the cvmfsexec attribute switch value from the config file
+glidein_cvmfsexec=`grep '^GLIDEIN_USE_CVMFSEXEC ' $glidein_config | awk '{print $2}'`
+# make the attribute value case insensitive
+#glidein_cvmfsexec=${glidein_cvmfsexec,,}
+echo "glidein_cvmfsexec value: $glidein_cvmfsexec"
 
 # get the CVMFS source information from <attr> in the glidein configuration 
 #cvmfs_source=osg
@@ -32,28 +38,25 @@ cvmfs_utils_dir=$work_dir/cvmfs_utils
 source $cvmfs_utils_dir/utils/cvmfs_helper_funcs.sh
 
 perform_system_check
+echo "GWMS_IS_CVMFS_MNT = $GWMS_IS_CVMFS_MNT"
 
 os_like=$GWMS_OS_DISTRO
 os_ver=`echo $GWMS_OS_VERSION | awk -F'.' '{print $1}'`
 arch=$GWMS_OS_KRNL_ARCH
 dist_file=cvmfsexec-${cvmfs_source}-${os_like}${os_ver}-${arch}
-#echo $dist_file
-
+		
 #tar -tvzf $cvmfs_utils_dir/utils/cvmfs_distros.tar.gz
 tar -xvzf $cvmfs_utils_dir/utils/cvmfs_distros.tar.gz -C $cvmfs_utils_dir distros/$dist_file
 
 . $cvmfs_utils_dir/utils/cvmfs_mount.sh
-
+	
 if [[ $GWMS_IS_CVMFS -eq 0 ]]; then
 	# CVMFS is now available on the worker node"
         # mimicking the behavior of the glidein on the worker node (start the user job once the CVMFS repositories are mounted)
 	loginfo "Starting user job..."
-#        . user_job.sh
+	# . user_job.sh
 else
-        logerror "CVMFS is still unavailable on the worker node"
+       	logerror "CVMFS is still unavailable on the worker node"
 	#Error occured during mount of CVMFS repositories"
 	exit 1
 fi
-
-
-#. $cvmfs_utils_dir/utils/cvmfs_unmount.sh
